bad4af009b300023f0792d5cc2a08c85674d5fed
sunil changoudnavar <sunilchangoudanaver@sitcoe.org.in>
2026-08-26 17:16:45 +0530

    Time: 0 ms (100%), Space: 46.2 MB (89.75%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-
+            // jar 1 kiva 0 nodes astil tr
         if(head == null || head.next == null){
             return head;
         }
@@ -20,14 +20,15 @@ class Solution {
         ListNode EvenHead = even;
 
         while(even != null && even.next != null){
+            //odd la connect kel
             odd.next = even.next;
             odd = odd.next;
-            
+            //even la connect kel ani te EvenHead madhe store ahe
             even.next = odd.next;
             even = even.next;
 
         }
-
+           //mg odd ans even conncet kel
         odd.next = EvenHead;
         return head;
     }
